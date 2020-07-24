15843241f39e2c6331fc9fcf18779e8d20f6be46
vineet.Kumar1 <vineet.kumar@silverpush.co>
2020-07-24 12:51:23 +0530

    org policies

---
+++ api/organisation/config/policies/requiredParams.js
@@ -0,0 +1,8 @@
+module.exports = async (ctx, next) => {
+  if (ctx.state.user.role.name === 'Administrator') {
+    // Go to next policy or will reach the controller's action.
+    return await next();
+  }
+
+  ctx.unauthorized(`You're not allowed to perform this action!`);
+};
