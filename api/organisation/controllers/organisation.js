ec2a2c694c1ce8982c7b3b15742810d5c8b4dc4d
vineet.Kumar1 <vineet.kumar@silverpush.co>
2020-07-24 10:47:30 +0530

    create organisation

--- api/organisation/controllers/organisation.js
+++ api/organisation/controllers/organisation.js
@@ -4,5 +4,50 @@
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
+const {
+    sanitizeEntity
+} = require('strapi-utils');
 
-module.exports = {};
+
+const formatError = error => [{
+    messages: [{
+      id: error.id,
+      message: error.message,
+      field: error.field
+    }]
+  }, ];
+
+module.exports = {
+    /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
+
+    async create(ctx) {
+        try {
+            let params = ctx.request.body;
+            params['account'] = ctx.state.user.account;
+            const organisation = await strapi.query('organisation').create(params);
+            console.log("organisation" , organisation)
+            const sanitizedOrg = sanitizeEntity(organisation.toJSON ? organisation.toJSON() : organisation, {
+                model: strapi.query('organisation').model,
+            });
+            console.log("sanitizedOrg" , sanitizedOrg)
+            return ctx.send({
+                organisation: sanitizedOrg,
+            });
+        } catch (error) {
+           return ctx.badRequest(null, formatError(error.message));
+        }
+    },
+    
+    async update(ctx) {
+        try {
+            let params = ctx.request.body;
+            
+        } catch (err) {
+            
+        }
+    }
+};
