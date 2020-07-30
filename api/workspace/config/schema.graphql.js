a313c3dd79a54f63328e8521edbdbaa1665bc921
amresh-yadav <amresh.yadav@dhwaniris.com>
2020-07-30 17:37:58 +0530

    workspaces: Graphql query and mutation

---
+++ api/workspace/config/schema.graphql.js
@@ -0,0 +1,51 @@
+const _ = require('lodash');
+module.exports = {
+    definition: `
+    type OrgWorkspace {
+      id: ID!,
+      name: String!,
+      short_name: String!,
+      organisation: Organisation
+    }
+    input OrgWorkspaceInput {
+        organisation:ID!,
+        name: String!,
+        short_name: String,
+        description:String
+    }
+  `,
+    query: `
+    orgWorkspaces: [OrgWorkspace]
+  `,
+    mutation:`
+        createOrgWorkspace(input: OrgWorkspaceInput): OrgWorkspace!,
+        updateOrgWorkspace(id: ID!, input: OrgWorkspaceInput): OrgWorkspace!
+    `,
+    resolver: {
+        Query: {
+            orgWorkspaces: {
+                policies: ['application::workspace.addFilter'],
+                resolver: 'application::workspace.workspace.find'
+            }
+        },
+        Mutation: {
+            createOrgWorkspace: async (obj, options, {
+                context
+            }) => {
+                context.params = _.toPlainObject(options);
+                context.request.body = _.toPlainObject(options.input);
+                return await strapi.controllers.workspace.create(context);
+            },
+            updateOrgWorkspace: async (obj, options, {
+              context
+            }) => {
+              context.params = _.toPlainObject(options);
+              context.request.body = _.toPlainObject(options.input);
+              console.log(context.params, context.request.body);
+              
+              return await strapi.controllers.workspace.update(context);
+            }
+        }
+    },
+    
+}
--- api/workspace/controllers/workspace.js
+++ api/workspace/controllers/workspace.js
@@ -7,4 +7,21 @@ const {
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    let entities;
+    try {
+      if (ctx.query && ctx.query._q) {
+        entities = await strapi.services.workspace.search(ctx.query, 'organisation');
+      } else {
+        entities = await strapi.services.workspace.find(ctx.query, 'organisation');
+      }
+      return entities.map(entity => sanitizeEntity(entity, {
+        model: strapi.models.workspace
+      }));
+      
+    } catch (error) {
+      console.log("Exception",error)
+    }
+  },
+};
