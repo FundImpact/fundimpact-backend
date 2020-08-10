1eca74b94aa8d208fc94557abd881d0cfa533efa
vineet.Kumar1 <vineet.kumar@silverpush.co>
2020-08-11 00:14:00 +0530

    deliverable trage and unit_org graphql

--- extensions/crm-plugin/controllers/organization.js
+++ extensions/crm-plugin/controllers/organization.js
@@ -9,39 +9,42 @@
  * @description: Village stores village information belonging to a specific district.
  */
 module.exports = {
-        find: async ctx => {
-            try {
-            // ctx.query._q: filter parameters in context object
-            if (ctx.query._q) {
-              // checks if any filter parameter is present
-              return await strapi.query("organization", "crm-plugin").search(ctx.query);
-            } else {
-              // returns all data if no filter parameter is passed
-              return await strapi.query("organization", "crm-plugin").find(ctx.query);
-            }
-          } catch (error) {
-            return ctx.badRequest(null, error.message);
-          }
-        },
-        findOne: async ctx => {
-          const { id } = ctx.params; // get id from context object
-          try {
-            return await strapi
-              .query("organization", "crm-plugin")
-              .findOne({ id });
-          } catch (error) {
-            return ctx.badRequest(null, error.message);
-          }
-        },
-        update: async ctx => {
-          try {
-            const { id } = ctx.params;
-            return await strapi
-              .query("organization", "crm-plugin")
-              .update({id}, ctx.request.body);
-            
-          } catch (error) {
-            return ctx.badRequest(null, error.message);
-          }
-        }
+  find: async ctx => {
+    try {
+      // ctx.query._q: filter parameters in context object
+      if (ctx.query._q) {
+        // checks if any filter parameter is present
+        return await strapi.query("organization", "crm-plugin").search(ctx.query);
+      } else {
+        // returns all data if no filter parameter is passed
+        console.log(ctx.query);
+        let result = await strapi.query("organization", "crm-plugin").find(ctx.query);
+        console.log(result);
+        return result;
+      }
+    } catch (error) {
+      return ctx.badRequest(null, error.message);
+    }
+  },
+  findOne: async ctx => {
+    const { id } = ctx.params; // get id from context object
+    try {
+      return await strapi
+        .query("organization", "crm-plugin")
+        .findOne({ id });
+    } catch (error) {
+      return ctx.badRequest(null, error.message);
+    }
+  },
+  update: async ctx => {
+    try {
+      const { id } = ctx.params;
+      return await strapi
+        .query("organization", "crm-plugin")
+        .update({ id }, ctx.request.body);
+
+    } catch (error) {
+      return ctx.badRequest(null, error.message);
+    }
+  }
 };
