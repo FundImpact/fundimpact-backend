0eb48b1809cdfbca9be6a977711bd42f76a9cada
vineet.Kumar1 <vineet.kumar@silverpush.co>
2020-08-13 19:52:15 +0530

    correction on filter

--- api/deliverable-category-unit/config/policies/addFilter.js
+++ api/deliverable-category-unit/config/policies/addFilter.js
@@ -1,8 +1,8 @@
 module.exports = async (ctx, next) => {
     try {
-        let deliverableUnitOrg = await strapi.query('deliverable-category-unit').find({ deliverable_category_org: ctx.query.deliverable_category_org });
+        let deliverableUnitOrg = await strapi.query('deliverable-category-org').find({ id: ctx.query.deliverable_category_org });
         Object.assign(ctx.query, {
-            deliverable_units_org: deliverableUnitOrg.map(m => m.id)
+            deliverable_category_org: deliverableUnitOrg.map(m => m.id)
         });
         return await next();
     } catch (err) {
