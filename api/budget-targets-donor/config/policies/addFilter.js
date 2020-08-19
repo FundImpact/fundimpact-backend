e3945f0256b727b43c9d1c55ee4a35089f4f761e
vineet.Kumar1 <vineet.kumar@silverpush.co>
2020-08-19 20:44:20 +0530

    add pagination for some models and add filters also

--- api/budget-targets-donor/config/policies/addFilter.js
+++ api/budget-targets-donor/config/policies/addFilter.js
@@ -1,9 +1,9 @@
 module.exports = async (ctx, next) => {
     try{
         let orgs = await strapi.query("organization", "crm-plugin").find({account:ctx.state.user.account});
-        let donors = await strapi.query("donor").find({organization_in: orgs.map(m => m.id) });
+        let donor = await strapi.query("donor").find({organization_in: orgs.map(m => m.id) });
         Object.assign(ctx.query, {
-            donor_in: donors.map(m => m.id)
+            donor_in: donor.map(m => m.id)
         });
         return await next() 
     }catch(err){
