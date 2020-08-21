6521ac09e2e10a0bc57473a0a425712f6c5cb210
vineet.Kumar1 <vineet.kumar@silverpush.co>
2020-08-21 21:48:16 +0530

    filter add in deliverable lineitem

--- api/budget-targets-project/config/schema.graphql.js
+++ api/budget-targets-project/config/schema.graphql.js
@@ -5,6 +5,7 @@ module.exports = {
   query: `
     projectBudgetTargets(sort: String , limit: Int, start: Int, where: JSON): [BudgetTargetsProject]
     projectBudgetTargetsCount(where : JSON) : Int!
+    projectBudgetTargetAmountSum(where : JSON) : Float!
   `,
   mutation: `
         createProjectBudgetTarget(input: BudgetTargetsProjectInput): BudgetTargetsProject!,
@@ -19,8 +20,15 @@ module.exports = {
       projectBudgetTargetsCount: {
         policies: ['application::budget-targets-project.addFilter'],
         resolver: 'application::budget-targets-project.budget-targets-project.count'
+      },
+      projectBudgetTargetAmountSum: async (obj, options, {
+        context
+      }) => {
+        context.params = _.toPlainObject(options);
+        context.request.body = _.toPlainObject(options.input);
+       
+        //return await strapi.controllers['budget-targets-project'].sumTarget(context);
       }
-
     },
     Mutation: {
       createProjectBudgetTarget: async (obj, options, {
