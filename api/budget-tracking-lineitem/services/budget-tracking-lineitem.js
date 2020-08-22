6c61abd31026be35981b791213dd00f682d5d9c9
vineet.Kumar1 <vineet.kumar@silverpush.co>
2020-08-22 18:43:18 +0530

    correction in spent amt calculation

--- api/budget-tracking-lineitem/services/budget-tracking-lineitem.js
+++ api/budget-tracking-lineitem/services/budget-tracking-lineitem.js
@@ -8,15 +8,12 @@
 module.exports = {
     totalSpendAmount: async ctx => {
         try {
-            
-            let budgetTargetsProject = await strapi.query("budget-targets-project").find({project: ctx.params.where.project});
-            let budgetTargetsProjectId = budgetTargetsProject.map(m => m.id)
-            let sumData = await strapi.connections.default.raw(`SELECT SUM(amount) FROM budget_tracking_lineitem where budget_targets_project IN (${budgetTargetsProjectId})`)
+            let sumData = await strapi.connections.default.raw(`SELECT SUM(amount) FROM budget_tracking_lineitem where budget_targets_project = ${ctx.params.where.budgetTargetsProject}`)
             return sumData.rows && sumData.rows.length > 0 && sumData.rows[0].sum != null ? sumData.rows[0].sum : 0;
         } catch (error) {
             console.log(error)
             return ctx.badRequest(null, error.message);
-            
+
         }
     }
 };
