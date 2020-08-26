a45f6bf5322db166b490c87f05ef901f8c5b4ae7
vineet.Kumar1 <vineet.kumar@silverpush.co>
2020-08-26 20:03:31 +0530

    fundImpact dashboard some changes

--- api/impact-target-project/services/impact-target-project.js
+++ api/impact-target-project/services/impact-target-project.js
@@ -5,4 +5,15 @@
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+    totalImpactAmount: async ctx => {
+        try {
+            let sumData = await strapi.connections.default.raw(`SELECT SUM(target_value) FROM impact_target_project where project = (${ctx.params.where.project})`)
+            return sumData.rows && sumData.rows.length > 0 && sumData.rows[0].sum != null ? sumData.rows[0].sum : 0;
+        } catch (error) {
+            console.log(error)
+            return ctx.badRequest(null, error.message);
+            
+        }
+    }
+};
