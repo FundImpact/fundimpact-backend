c15a36c74fd0a936d2ca362607c6359988c28d17
vineet <vineetkmr92@gmail.com>
2020-09-11 17:53:18 +0530

    project count with category

--- api/budget-category-organization/config/schema.graphql.js
+++ api/budget-category-organization/config/schema.graphql.js
@@ -1,41 +1,51 @@
 const _ = require('lodash');
 module.exports = {
-    definition: `
+  definition: `
   `,
-    query: `
+  query: `
     orgBudgetCategory(sort: String , limit: Int, start: Int, where: JSON): [BudgetCategoryOrganization]
     orgBudgetCategoryCount(where : JSON) : Int!
+    projectCountBudgetCatByOrg(where : JSON) : JSON!  
   `,
-    mutation:`
+  mutation: `
         createOrgBudgetCategory(input: BudgetCategoryOrganizationInput): BudgetCategoryOrganization!,
         updateOrgBudgetCategory(id: ID!, input: BudgetCategoryOrganizationInput): BudgetCategoryOrganization!
     `,
-    resolver: {
-        Query: {
-          orgBudgetCategory: {
-                policies: ['application::budget-category-organization.addFilter'],
-                resolver: 'application::budget-category-organization.budget-category-organization.find'
-          },
-          orgBudgetCategoryCount: {
-              policies: ['application::budget-category-organization.addFilter'],
-              resolver: 'application::budget-category-organization.budget-category-organization.count'
-          }
-        },
-        Mutation: {
-          createOrgBudgetCategory: async (obj, options, {
-                context
-            }) => {
-                context.params = _.toPlainObject(options);
-                context.request.body = _.toPlainObject(options.input);
-                return await strapi.controllers['budget-category-organization'].create(context);
-            },
-            updateOrgBudgetCategory: async (obj, options, {
-              context
-            }) => {
-              context.params = _.toPlainObject(options);
-              context.request.body = _.toPlainObject(options.input);
-              return await strapi.controllers['budget-category-organization'].update(context);
-            }
+  resolver: {
+    Query: {
+      orgBudgetCategory: {
+        policies: ['application::budget-category-organization.addFilter'],
+        resolver: 'application::budget-category-organization.budget-category-organization.find'
+      },
+      orgBudgetCategoryCount: {
+        policies: ['application::budget-category-organization.addFilter'],
+        resolver: 'application::budget-category-organization.budget-category-organization.count'
+      },
+      projectCountBudgetCatByOrg: {
+        policies: ['application::budget-category-organization.addFilter'],
+        resolverOf: 'application::budget-category-organization.budget-category-organization.project_count_budget_cat_by_org',
+        resolver: async (obj, options, { context }) => {
+          context.params = _.toPlainObject(options);
+          context.request.body = _.toPlainObject(options.input);
+          return await strapi.controllers['budget-category-organization'].project_count_budget_cat_by_org(context);
         }
-    }   
+      }
+    },
+    Mutation: {
+      createOrgBudgetCategory: async (obj, options, {
+        context
+      }) => {
+        context.params = _.toPlainObject(options);
+        context.request.body = _.toPlainObject(options.input);
+        return await strapi.controllers['budget-category-organization'].create(context);
+      },
+      updateOrgBudgetCategory: async (obj, options, {
+        context
+      }) => {
+        context.params = _.toPlainObject(options);
+        context.request.body = _.toPlainObject(options.input);
+        return await strapi.controllers['budget-category-organization'].update(context);
+      }
+    }
+  }
 }
