15c486f2e3361e43e20d3f55ca50263e3b57dee0
vineet <vineetkmr92@gmail.com>
2020-09-21 22:38:20 +0530

    deliverable dashboard api

--- api/deliverable-category-org/controllers/deliverable-category-org.js
+++ api/deliverable-category-org/controllers/deliverable-category-org.js
@@ -69,4 +69,17 @@ module.exports = {
             return ctx.badRequest(null, error.message);
         }
     },
+    avgAchivementDeliverableByOrg :  async ctx => {
+        try {
+            let data = await strapi.connections.default.raw(`WITH cte AS(select sum(dtp.target_value) as sum_dtp ,  
+            sum(dtl.value) as sum_dtl from deliverable_category_org dco JOIN deliverable_category_unit dcu ON  dco.id = dcu.deliverable_category_org 
+            JOIN deliverable_target_project dtp ON dtp.deliverable_category_unit = dcu.id 
+            JOIN deliverable_tracking_lineitem dtl ON dtp.id = dtl.deliverable_target_project  where organization = ${ctx.query.organization}) 
+            select ROUND((sum_dtl * 100.0)/ sum_dtp) as avg from cte where cte.sum_dtp <> cte.sum_dtl`)
+            return data.rows && data.rows.length > 0 && data.rows[0].avg  ? data.rows[0].avg : 0;
+        } catch (error) {
+            console.log(error)
+            return ctx.badRequest(null, error.message);
+        }
+    },
 };
