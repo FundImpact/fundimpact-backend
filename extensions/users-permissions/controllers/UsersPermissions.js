82a1a7222a48973ea0e176c97099f22800fd3be3
Pvinit <vineetkmr92@gmail.com>
2020-09-30 13:24:13 +0530

    get rolw permission

--- extensions/users-permissions/controllers/UsersPermissions.js
+++ extensions/users-permissions/controllers/UsersPermissions.js
@@ -1,11 +1,11 @@
 const _ = require('lodash');
-const permissionsService =  require('../../../services/helper/permissions');
+const permissionsService = require('../../../services/helper/permissions');
 module.exports = {
-  /**
-   * Default action.
-   *
-   * @return {Object}
-   */
+    /**
+     * Default action.
+     *
+     * @return {Object}
+     */
     async createOrganizationRole(ctx) {
         if (_.isEmpty(ctx.request.body)) {
             return ctx.throw(400, `Cannot be empty.`);
@@ -14,22 +14,22 @@ module.exports = {
         try {
             let payload = ctx.request.body.input;
             const organization = ctx.state.user.organization;
-            const permissions = payload.permissions ? payload.permissions:await permissionsService.createAdminPermissions();
+            const permissions = payload.permissions ? payload.permissions : await permissionsService.createAdminPermissions();
             const type = `${_.snakeCase(_.deburr(_.toLower(payload.name)))}-org-${organization}`;
             const roleParams = {
-                name:payload.name,
-                type:type,
-                users:[],
-                organization:organization,
-                description:`Role (${type}).`,
-                permissions:permissions
+                name: payload.name,
+                type: type,
+                users: [],
+                organization: organization,
+                description: `Role (${type}).`,
+                permissions: permissions
             };
             let roleQuery = {
-                type: roleParams.type, 
-                organization:roleParams.organization
+                type: roleParams.type,
+                organization: roleParams.organization
             }
-            const roleExists = await strapi.query('role', 'users-permissions').findOne(roleQuery,[]);
-            if(roleExists){
+            const roleExists = await strapi.query('role', 'users-permissions').findOne(roleQuery, []);
+            if (roleExists) {
                 return ctx.throw(400, `Requested role is already exists.`);
                 // return ctx.badRequest(null, [{ messages: [`Requested role is already exists.`] }]);
             }
@@ -37,7 +37,7 @@ module.exports = {
             await strapi.plugins['users-permissions'].services.userspermissions.createRole(
                 roleParams
             );
-            const role = await strapi.query('role', 'users-permissions').findOne(roleQuery,[]);
+            const role = await strapi.query('role', 'users-permissions').findOne(roleQuery, []);
             ctx.send(role);
         } catch (err) {
             strapi.log.error(err);
@@ -47,12 +47,21 @@ module.exports = {
     },
     async getOrganizationRoles(ctx) {
         try {
-            Object.assign(ctx.request.query,{organization:ctx.state.user.organization});
+            Object.assign(ctx.request.query, { organization: ctx.state.user.organization });
             const roles = await strapi.query('role', 'users-permissions').find(ctx.request.query, []);
-            console.log(roles);
             ctx.send(roles);
         } catch (err) {
             //ctx.badRequest(null, [{ messages: [{ id: 'Not found' }] }]);
+            return ctx.throw(400, err);
+        }
+    },
+
+    async getRolePemissions(ctx) {
+        try {
+            ctx.request.query._limit = -1;
+            const permissions = await strapi.query('permission', 'users-permissions').find(ctx.request.query, []);
+            ctx.send(permissions);
+        } catch (err) {
             return ctx.throw(400, err);
         }
     }
