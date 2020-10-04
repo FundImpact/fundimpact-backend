fc60df4f555921837b187cfc6b3ec819cc988ac9
amresh-yadav <amresh.yadav@dhwaniris.com>
2020-10-04 16:09:32 +0530

    added role update/count api

--- extensions/users-permissions/controllers/UsersPermissions.js
+++ extensions/users-permissions/controllers/UsersPermissions.js
@@ -1,5 +1,6 @@
 const _ = require('lodash');
 const permissionsService = require('../../../services/helper/permissions');
+const { count } = require('./Auth');
 module.exports = {
     /**
      * Default action.
@@ -25,15 +26,12 @@ module.exports = {
                 permissions: permissions
             };
             let roleQuery = {
-                type: roleParams.type,
-                organization: roleParams.organization
+                type: roleParams.type
             }
             const roleExists = await strapi.query('role', 'users-permissions').findOne(roleQuery, []);
             if (roleExists) {
                 return ctx.throw(400, `Requested role is already exists.`);
-                // return ctx.badRequest(null, [{ messages: [`Requested role is already exists.`] }]);
             }
-
             await strapi.plugins['users-permissions'].services.userspermissions.createRole(
                 roleParams
             );
@@ -42,7 +40,31 @@ module.exports = {
         } catch (err) {
             strapi.log.error(err);
             return ctx.throw(400, err);
-            //ctx.badRequest(null, [{ messages: [{ id: 'An error occured' }] }]);
+        }
+    },
+    async updateOrganizationRole(ctx) {
+        if (_.isEmpty(ctx.request.body)) {
+            return ctx.throw(400, `Cannot be empty.`);
+            // return ctx.badRequest(null, [{ messages: [{ id: 'Cannot be empty' }] }]);
+        }
+        try {
+            let roleId = ctx.request.body.id;
+            let payload = ctx.request.body.input;
+            if(!roleId){
+                return ctx.throw(400, `'id' is required field.`)
+            }
+            if(!payload.permissions){
+                delete payload.permissions;
+            }
+            await strapi.plugins['users-permissions'].services.userspermissions.updateRole(
+                roleId,
+                payload
+            );
+            const role = await strapi.query('role', 'users-permissions').findOne({id:roleId}, []);
+            ctx.send(role);
+        } catch (err) {
+            strapi.log.error(err);
+            return ctx.throw(400, err);
         }
     },
     async getOrganizationRoles(ctx) {
@@ -55,13 +77,22 @@ module.exports = {
             return ctx.throw(400, err);
         }
     },
-
     async getRolePemissions(ctx) {
         try {
             ctx.request.query._limit = -1;
             const permissions = await strapi.query('permission', 'users-permissions').find(ctx.request.query, []);
             ctx.send(permissions);
         } catch (err) {
+            return ctx.throw(400, err);
+        }
+    },
+    async getOrganizationRolesCount(ctx){
+        try {
+            Object.assign(ctx.request.query, { organization: ctx.state.user.organization });
+            const count = await strapi.query('role', 'users-permissions').count(ctx.request.query, []);
+            ctx.send({count});
+        } catch (err) {
+            //ctx.badRequest(null, [{ messages: [{ id: 'Not found' }] }]);
             return ctx.throw(400, err);
         }
     }
