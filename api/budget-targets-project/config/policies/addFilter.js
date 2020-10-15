341faa2fcb17e5ea037a2cf1028021d0faf6763e
Pvinit <vineetkmr92@gmail.com>
2020-10-15 22:32:38 +0530

    project filter

--- api/budget-targets-project/config/policies/addFilter.js
+++ api/budget-targets-project/config/policies/addFilter.js
@@ -1,8 +1,9 @@
 module.exports = async (ctx, next) => {
     try {
-        //let orgs = await strapi.query("organization", "crm-plugin").find({ account: ctx.state.user.account });
-        //let wps = await strapi.query("workspace").find({ organization_in: orgs.map(m => m.id) });
-        let projects = await strapi.query("project").find({ id :  ctx.query.project});
+       
+        let orgs = await strapi.query("organization", "crm-plugin").find({ account: ctx.state.user.account });
+        let wps = await strapi.query("workspace").find({ organization_in: orgs.map(m => m.id) });
+        let projects = await strapi.query("project").find({ workspace: wps.map(m => m.id) });
         Object.assign(ctx.query, {
             project_in: projects.map(m => m.id)
         });
--- api/impact-units-org/config/policies/addFilter.js
+++ api/impact-units-org/config/policies/addFilter.js
@@ -1,8 +1,9 @@
 module.exports = async (ctx, next) => {
     try{
         let orgs = await strapi.query("organization", "crm-plugin").find({account:ctx.state.user.account});
+        orgs = orgs.map(m => m.id)
         Object.assign(ctx.query, {
-            organization_in: orgs.map(m => m.id)
+            organization_in: ctx.state.user.role && ctx.state.user.role.is_project_level === true ? [] : orgs 
         });
         return await next();
     }catch(err){
