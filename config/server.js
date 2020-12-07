89943e3b2411d7b1079d5098997ceaed79c49f34
realgcoding <realgcoding@gmail.com>
2020-12-07 14:33:10 +0530

    changed localhost port

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  url:  env('URL',`http://localhost:3000`)
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: env("URL", `http://localhost:1337`),
 });
