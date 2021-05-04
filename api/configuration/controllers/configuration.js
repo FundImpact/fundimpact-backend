61a53d7e36ecd95b8a41578066021af9788bcb96
shubhamsingh3211 <69340232+shubhamsingh3211@users.noreply.github.com>
2021-05-04 11:12:12 +0530

    functionality added to fetch roles from one env and submit in other env

--- api/configuration/controllers/configuration.js
+++ api/configuration/controllers/configuration.js
@@ -1,20 +1,66 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
-const _ = require('lodash');
-const permissions = require('../../../services/helper/permissions');
-const permissionsService =  require('../../../services/helper/permissions');
+const _ = require("lodash");
+const permissions = require("../../../services/helper/permissions");
+const permissionsService = require("../../../services/helper/permissions");
+const axios = require("axios");
+
 module.exports = {
-    async permissions(ctx) {
-        try {
-            let permissions = await permissionsService.createAdminPermissions({enabled:false,purpose:"ORGROLECREATION"});
-            ctx.send({permissions});
-        } catch (err) {
-            strapi.log.error(err);
-            return ctx.throw(400, err);
-        }
+  async permissions(ctx) {
+    try {
+      let permissions = await permissionsService.createAdminPermissions({
+        enabled: false,
+        purpose: "ORGROLECREATION",
+      });
+      ctx.send({ permissions });
+    } catch (err) {
+      strapi.log.error(err);
+      return ctx.throw(400, err);
     }
+  },
+  async fetchRolesFromOneEnvAndSubmitInOtherEnv(ctx) {
+    try {
+      const {
+        request: {
+          body: { fetchRolesFrom, roles, authorizationHeader },
+        },
+      } = ctx;
+      const fetchedRoles = await Promise.all(
+        roles.map((role) =>
+          axios.get(`${fetchRolesFrom}/users-permissions/roles/${role}`, {
+            headers: {
+              Authorization: authorizationHeader,
+            },
+          })
+        )
+      );
+      await createFetchedRoles(fetchedRoles);
+      return ctx.send({ message: "roles created" });
+    } catch (err) {
+      strapi.log.error(err);
+      return ctx.throw(400, err);
+    }
+  },
+};
+
+const createFetchedRoles = async (fetchedRoles) => {
+  for (const fetchedRole of fetchedRoles) {
+    const {
+      data: { role },
+    } = fetchedRole;
+    await strapi.plugins[
+      "users-permissions"
+    ].services.userspermissions.createRole({
+      name: role.name,
+      description: role.description,
+      type: role.type,
+      is_project_level: role.is_project_level,
+      sequence: role.sequence,
+      permissions: role.permissions,
+    });
+  }
 };
