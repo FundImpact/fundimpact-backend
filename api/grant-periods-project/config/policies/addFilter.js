29f0578959c306d57db188a363f0e6b88a631dae
shubhamsingh3211 <69340232+shubhamsingh3211@users.noreply.github.com>
2021-05-07 21:39:06 +0530

    grant period donor fetch issue resolved

--- api/grant-periods-project/config/policies/addFilter.js
+++ api/grant-periods-project/config/policies/addFilter.js
@@ -1,17 +1,34 @@
 module.exports = async (ctx, next) => {
-    try {
-        let orgs = await strapi.query("organization").find({ account: ctx.state.user.account });   
-        let wps = await strapi.query("workspace").find({ organization_in: orgs.map(m => m.id) });
-        let projects = await strapi.query("project").find({ workspace_in: wps.map(m => m.id) });
-        let donors = await strapi.query("donor").find({ organization_in: orgs.map(m => m.id) });
-        Object.assign(ctx.query, {
-            project_in: projects.map(m => m.id),
-            donor_in: donors.map(m => m.id),
-            deleted: false
-        });
-        Object.keys(ctx.query).forEach((key) => (Array.isArray(ctx.query[key]) && ctx.query[key].length == 0  ? delete ctx.query[key] : ctx.query[key]))
-        return await next()
-    } catch (err) {
-        ctx.badRequest(`Error occured - ${err.message}`);
-    }
+  try {
+    const knex = strapi.connections.default;
+    const orgs = await knex("organizations").where(
+      "account",
+      ctx.state.user.account
+    );
+    const wps = await knex("workspaces").whereIn(
+      "organization",
+      orgs.map((m) => m.id)
+    );
+    const projects = await knex("projects").whereIn(
+      "workspace",
+      wps.map((m) => m.id)
+    );
+    const donors = await knex("donors").whereIn(
+      "organization",
+      orgs.map((m) => m.id)
+    );
+    Object.assign(ctx.query, {
+      project_in: projects.map((m) => m.id),
+      donor_in: donors.map((m) => m.id),
+      deleted: false,
+    });
+    Object.keys(ctx.query).forEach((key) =>
+      Array.isArray(ctx.query[key]) && ctx.query[key].length == 0
+        ? delete ctx.query[key]
+        : ctx.query[key]
+    );
+    return await next();
+  } catch (err) {
+    ctx.badRequest(`Error occured - ${err.message}`);
+  }
 };
