d5b445ca7aa655c07c5222a7f522b6f61f360f21
shubhamsingh3211 <69340232+shubhamsingh3211@users.noreply.github.com>
2021-05-11 11:49:13 +0530

    limit increased

--- api/project-donor/config/policies/addFilter.js
+++ api/project-donor/config/policies/addFilter.js
@@ -2,7 +2,7 @@ module.exports = async (ctx, next) => {
     try{
         let orgs = await strapi.query("organization").find({account:ctx.state.user.account});
         let wps = await strapi.query("workspace").find({organization_in:orgs.map(m => m.id)});
-        let donors = await strapi.query("donor").find({organization_in:orgs.map(m => m.id)});
+        let donors = await strapi.query("donor").find({organization_in:orgs.map(m => m.id), _limit: 1000 });
         let projects = await strapi.query("project").find({workspace_in:wps.map(m => m.id)});
         Object.assign(ctx.query, {
             project_in: projects.map(m => m.id),
