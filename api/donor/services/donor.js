57360e182748cd34c67efa4a9f52ea69a6b14585
shubhamsingh3211 <69340232+shubhamsingh3211@users.noreply.github.com>
2021-05-19 12:12:02 +0530

    delete project donor functionality added

--- api/donor/services/donor.js
+++ api/donor/services/donor.js
@@ -1,8 +1,40 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/services.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+const checkIfDonorBelongToProjectDonor = async (donorId, ctx) => {
+  const knex = strapi.connections.default;
+  let orgs = await strapi
+    .query("organization")
+    .find({ account: ctx.state.user.account });
+  let wps = await strapi
+    .query("workspace")
+    .find({ organization_in: orgs.map((m) => m.id), _limit: 1000 });
+  let projects = await strapi
+    .query("project")
+    .find({ workspace: wps.map((m) => m.id), _limit: 1000 });
+  const projectDonor = await knex("project_donor")
+    .where({ donor: donorId, deleted: false })
+    .whereIn(
+      "project",
+      projects.map((project) => project.id)
+    );
+  return projectDonor && projectDonor.length;
+};
+
+const checkIfDonorBelongToTableProvided = async (tableName, donorId) => {
+  const knex = strapi.connections.default;
+  const tableRowsWithGivenDonor = await knex(tableName).where({
+    donor: donorId,
+    deleted: false
+  });
+  return tableRowsWithGivenDonor && tableRowsWithGivenDonor.length;
+};
+
+module.exports = {
+  checkIfDonorBelongToProjectDonor,
+  checkIfDonorBelongToTableProvided
+};
