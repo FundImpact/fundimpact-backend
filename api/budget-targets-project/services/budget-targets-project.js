065799454727e953c3de9e30bb19aec0c90148b6
arjun_dhwani <arjun.malik@dhwaniris.com>
2021-08-31 14:37:22 +0530

    changes

--- api/budget-targets-project/services/budget-targets-project.js
+++ api/budget-targets-project/services/budget-targets-project.js
@@ -8,7 +8,7 @@
 module.exports = {
     totalTargetExpense: async ctx => {
         try {
-            let sumData = await strapi.connections.default.raw(`SELECT SUM(target_value) FROM budget_sub_targets where project = ${ctx.params.where.project} and COALESCE(budget_targets_project.deleted, false) <> true`)
+            let sumData = await strapi.connections.default.raw(`SELECT SUM(target_value) FROM budget_sub_targets where project = ${ctx.params.where.project} and COALESCE(budget_sub_targets.deleted, false) <> true`)
             return sumData.rows && sumData.rows.length > 0 && sumData.rows[0].sum != null ? sumData.rows[0].sum : 0;
         } catch (error) {
             return ctx.badRequest(null, error.message);
