1338ada47eb04ef8c9eb48176dafdc6f660d5a13
arjun_dhwani <arjun.malik@dhwaniris.com>
2021-09-10 16:57:14 +0530

    add graphql query

--- api/deliverable-sub-target/services/deliverable-sub-target.js
+++ api/deliverable-sub-target/services/deliverable-sub-target.js
@@ -5,4 +5,33 @@
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+
+    DeliverableSubTargetsConnection: async ctx => {
+        try {
+            let sumData = await strapi.connections.default.raw(`SELECT SUM(target_value),count(id) FROM deliverable_sub_targets 
+                where COALESCE(deleted, false) <> true`)
+
+            if(sumData.rows && sumData.rows.length > 0){
+                let obj = {
+                    "deliverableSubTargetsConnection":{ 
+                        "aggregate":{
+                        "sum": {
+                        "target_value":sumData.rows[0].sum != null ? sumData.rows[0].sum:0
+                        },
+                        "count": sumData.rows[0].count != null ? sumData.rows[0].count:0
+                        }
+                    }    
+                }
+                return obj
+            }
+            return 0     
+        } catch (error) {
+            console.log(error)
+            return ctx.badRequest(null, error.message);
+            
+        }
+    },
+
+
+};
