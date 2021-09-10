5e637a70e2f8d80978a57f12b071990c0d383c6a
arjun_dhwani <arjun.malik@dhwaniris.com>
2021-09-10 12:59:23 +0530

    changes

--- api/deliverable-target-project/services/deliverable-target-project.js
+++ api/deliverable-target-project/services/deliverable-target-project.js
@@ -9,10 +9,10 @@ module.exports = {
     totalDeliverableAmount: async ctx => {
         try {
 
-            console.log(ctx.params.where.deliverable_target_project.type)
+            let type = ctx.params.where.deliverable_target_project.type;
             let sumData = await strapi.connections.default.raw(`SELECT SUM(deliverable_sub_targets.target_value) FROM deliverable_sub_targets 
             INNER JOIN deliverable_target_project dtp on dtp.id = deliverable_sub_targets.deliverable_target_project
-            where deliverable_sub_targets.project = ${ctx.params.where.project} and dtp.type = ${ctx.params.where.deliverable_target_project.type}   and COALESCE(deleted, false) <> true`)
+            where deliverable_sub_targets.project = ${ctx.params.where.project} and dtp.type = '${type}' and COALESCE(deleted, false) <> true`)
             return sumData.rows && sumData.rows.length > 0 && sumData.rows[0].sum != null ? sumData.rows[0].sum : 0;
         } catch (error) {
             console.log(error)
