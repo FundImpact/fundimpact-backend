d48a039f59e4e68bea3c58f4cec8ab04b692add2
arjun_dhwani <arjun.malik@dhwaniris.com>
2021-09-10 13:38:06 +0530

    changes

--- api/deliverable-target-project/config/schema.graphql.js
+++ api/deliverable-target-project/config/schema.graphql.js
@@ -27,7 +27,7 @@ module.exports = {
             },
             deliverableAchieved: {
                 policies: ['application::deliverable-target-project.addFilter'],
-                resolverOf: 'application::deliverable-target-project.deliverable-target-project.deliverable_achieved',
+                resolverOf: 'application::deliverable-target-project.deliverable-target-project.totalDeliverableAchieved',
                 resolver: async (obj, options, { context }) => {
                   context.params = _.toPlainObject(options);
                   context.request.body = _.toPlainObject(options.input);
