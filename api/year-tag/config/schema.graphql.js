cd4266bba31263c48bcf0f76affe1524ac57cad1
Suresh Mali <suruchi.singhal@dhwaniris.in>
2021-11-18 13:17:07 +0530

    bug fix

--- api/year-tag/config/schema.graphql.js
+++ api/year-tag/config/schema.graphql.js
@@ -1,7 +1,11 @@
+const _ = require('lodash');
 module.exports = {
+    definition: ``,
     query: `
       yearTagCount(where: JSON): Int!
-    `,
+      yearTagDonor(id: ID)
+      yearTagOrganization(id: ID)`,
+    mutation: ``,
     resolver: {
         Query: {
             yearTagCount: {
@@ -11,6 +15,35 @@ module.exports = {
                     return await strapi.api['year-tag'].services['year-tag'].count(options.where || {});
                 },
             },
+            yearTagDonor: {
+                resolver: 'application::year-tag.year-tag.donerWiseYearTag'
+            },
+            yearTagOrganization: {
+                resolver: 'application::year-tag.year-tag.organizationWiseYearTag'
+            }
         },
     },
 };
+
+
+// module.exports = {
+//     query: `
+//       yearTagCount(where: JSON): Int!
+//     `,
+//     resolver: {
+//         Query: {
+//             yearTagCount: {
+//                 description: 'Return the count of year tag',
+//                 resolverOf: 'application::year-tag.count',
+//                 resolver: async (obj, options, ctx) => {
+//                     return await strapi.api['year-tag'].services['year-tag'].count(options.where || {});
+//                 },
+//             }
+//         },
+//     },
+// };
+
+
+// yearTagDonor: {
+//     resolver: 'application::year-tag.donerWiseYearTag'
+// }
