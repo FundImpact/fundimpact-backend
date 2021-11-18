bffd81a2eb2264c977e3cb70c3e652c32183169a
Suresh Mali <suruchi.singhal@dhwaniris.in>
2021-11-18 14:28:14 +0530

    year tag name ad

--- api/year-tag/config/schema.graphql.js
+++ api/year-tag/config/schema.graphql.js
@@ -24,26 +24,3 @@ module.exports = {
         },
     },
 };
-
-
-// module.exports = {
-//     query: `
-//       yearTagCount(where: JSON): Int!
-//     `,
-//     resolver: {
-//         Query: {
-//             yearTagCount: {
-//                 description: 'Return the count of year tag',
-//                 resolverOf: 'application::year-tag.count',
-//                 resolver: async (obj, options, ctx) => {
-//                     return await strapi.api['year-tag'].services['year-tag'].count(options.where || {});
-//                 },
-//             }
-//         },
-//     },
-// };
-
-
-// yearTagDonor: {
-//     resolver: 'application::year-tag.donerWiseYearTag'
-// }
