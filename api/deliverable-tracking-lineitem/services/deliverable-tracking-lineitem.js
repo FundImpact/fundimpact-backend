1ef03af4baef1c4e2a85ee87c47d86afacd25a4c
Suresh Mali <suruchi.singhal@dhwaniris.in>
2021-12-15 12:06:43 +0530

    sum

--- api/deliverable-tracking-lineitem/services/deliverable-tracking-lineitem.js
+++ api/deliverable-tracking-lineitem/services/deliverable-tracking-lineitem.js
@@ -18,9 +18,10 @@ module.exports = {
     totalValue: async ctx => {
         try {
             let totalAmt = await strapi.connections.default.raw(`
-            SELECT SUM(dtp.value) as value FROM deliverable_tracking_lineitem  dstli
-            LEFT JOIN deliverable_target_project dtp on dtp.id = dstli.deliverable_target_project
-            where dtp.project =${ctx.params.where.project} and dtp.type ='${ctx.params.where.type}' and dtp.id=${ctx.params.where.deliverable_target_project} and COALESCE(dtp.deleted, false) <> true`)
+            select SUM(dstli.value) as value from deliverable_target_project dtp 
+            left join (SELECT * FROM  deliverable_sub_targets  WHERE deleted=false) as dst ON dtp.id=dst.deliverable_target_project
+            left join (SELECT SUM(id) as value, deliverable_sub_target FROM  deliverable_tracking_lineitem WHERE deleted=false group by deliverable_sub_target) as dstli ON dst.id=dstli.deliverable_sub_target
+            where type='${ctx.params.where.type}' and dtp.project=${ctx.params.where.project} and dtp.id=${ctx.params.where.deliverable_target_project} and dtp.deleted=false`)
             return totalAmt.rows && totalAmt.rows.length > 0 && totalAmt.rows[0].value !== null ? totalAmt.rows[0].value : 0;
         } catch (error) {
             console.log(error)
@@ -32,9 +33,8 @@ module.exports = {
             let totalAmt = await strapi.connections.default.raw(`
             select SUM(target_value) as target_total,SUM(dstli.value) as achieved_total from deliverable_target_project dtp 
             left join (SELECT * FROM  deliverable_sub_targets  WHERE deleted=false) as dst ON dtp.id=dst.deliverable_target_project
-            left join (SELECT * FROM  deliverable_tracking_lineitem WHERE deleted=false) as dstli ON dtp.id=dstli.deliverable_target_project
+            left join (SELECT SUM(id) as value, deliverable_sub_target FROM  deliverable_tracking_lineitem WHERE deleted=false group by deliverable_sub_target) as dstli ON dst.id=dstli.deliverable_sub_target
             where type='${ctx.params.where.type}' and dtp.project=${ctx.params.where.project} and dtp.deleted=false`)
-            // console.log("totalAmt",totalAmt)
             return totalAmt.rows && totalAmt.rows.length > 0 ? totalAmt.rows[0] : {};
         } catch (error) {
             console.log(error)
